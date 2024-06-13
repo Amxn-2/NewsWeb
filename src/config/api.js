8d8c3beb15d186f308853e85a76e81c611b790e3
Amxn-2 <amansharmaa1205@gmail.com>
2024-06-13 16:14:18 +0530

    final

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
 const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
-const API_KEY = "YOUR-API-KEY";
+const API_KEY = "02e576f0f920d92e70009abeab5cadb5";
 export const endpointPath = (country, category) =>
   `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
 export const endpointSearch = (searchQuery) =>
